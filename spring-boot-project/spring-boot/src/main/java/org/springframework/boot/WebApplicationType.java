5e14db3333201fc70cdb152f6f77a6bde6fdf27d
machenggong1996 <mcg1996@163.com>
2020-12-01 09:57:36 +0800

    springboot添加注释

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/WebApplicationType.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/WebApplicationType.java
@@ -30,18 +30,21 @@ public enum WebApplicationType {
 	/**
 	 * The application should not run as a web application and should not start an
 	 * embedded web server.
+	 * 非web项目
 	 */
 	NONE,
 
 	/**
 	 * The application should run as a servlet-based web application and should start an
 	 * embedded servlet web server.
+	 * servlet web 项目
 	 */
 	SERVLET,
 
 	/**
 	 * The application should run as a reactive web application and should start an
 	 * embedded reactive web server.
+	 * 响应式 web 项目
 	 */
 	REACTIVE;
 
